4dd741f30f9042cd5f6e037807d8c992a1887b33
jjz <jjz@ubuntu-14.04>
2016-08-25 17:17:46 +0800

    delete all

--- gdb/functions.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
- int function(void)
- {
-    std::cout << "I am in a function!" << std::endl;
-    return 0;
- }
--- gdb/testobj.h
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int TestObj(void)
-{
-        std::cout << "I am in TestObj!" << std::endl;
-            return 0;
-}
