12600ea5fd431ab1dd05dd8388165d107158ace5
jjz <jjz@ubuntu-14.04>
2016-08-26 17:01:42 +0800

    modify aio test

--- aio.cpp
+++ aio.cpp
@@ -6,8 +6,8 @@
 #include <fcntl.h>
 #include <errno.h>
 using namespace std;
-Buffer::Buffer(char*addr, size_t size, size_t id)
-	:mBuf(addr),mBufSize(size),mId(id)
+	Buffer::Buffer(char*addr, size_t size, size_t id)
+:mBuf(addr),mBufSize(size),mId(id)
 {
 	mFd = 0;
 	mOrigOffset = 0;
@@ -18,14 +18,14 @@ Buffer::Buffer(char*addr, size_t size, size_t id)
 Buffer::~Buffer()
 {
 }
-	
+
 BufferUser::BufferUser()
 {
 
 }
 
-BufferUser::BufferUser(Buffer* buf)
-	:mBuf(buf),mOffset(0)
+	BufferUser::BufferUser(Buffer* buf)
+:mBuf(buf),mOffset(0)
 {
 
 }
@@ -56,13 +56,12 @@ int Center::start()
 {
 	char* addr = (char*)memalign( bufsize,bufsize*mBufferCount);
 	if (addr != NULL){
-	//cout<<"memalign len: "<<strlen(addr)<<endl;
-	for (int i =0; i<mBufferCount;i++)
-	{
-		Buffer* buf = new Buffer(&addr[i*bufsize],bufsize,i+1);
-		modifyReq(buf,i);
-		addBuffer(buf);
-	}
+		for (int i =0; i<mBufferCount;i++)
+		{
+			Buffer* buf = new Buffer(&addr[i*bufsize],bufsize,i+1);
+			modifyReq(buf,i);
+			//			addBuffer(buf);
+		}
 	}
 }
 
@@ -84,9 +83,9 @@ void Center::addBuffer(Buffer* buf)
 
 void Center::modifyReq(Buffer* buf, int i)
 {
-	size_t len = strlen(bufstr[2*i]);
-	strcpy(buf->mBuf,bufstr[2*i]);
-	strcpy(buf->mBuf+len,bufstr[2*i+1]);
+	//	size_t len = strlen(bufstr[2*i]);
+	//	strcpy(buf->mBuf,bufstr[2*i]);
+	//	strcpy(buf->mBuf+len,bufstr[2*i+1]);
 	buf->reqId = i+1;
 	BufferUser bu(buf);
 	mList.push_back(bu);
@@ -105,6 +104,7 @@ void Center::testBuffer()
 
 void Center::aiosubmit()
 {
+	cout<<mList.size()<<endl;
 	BufferList bufs;
 	bufs.swap(mList);
 	vector<struct iocb>iocbArray;
@@ -114,30 +114,45 @@ void Center::aiosubmit()
 	iocbArray.clear();
 	iocbs.clear();
 	BufferList::iterator it = bufs.begin();
+	int fd = open("TianShanBuild.sln",O_RDONLY);
 	size_t pos = 0;
-	int fd = open("file",O_RDONLY);
-	cout<<"fd: "<<fd<<endl;
-	struct iocb* cb = &iocbArray[pos];
-	io_prep_pread( cb, fd, it->mBuf, bufsize, 0 );
-	cb->data = it->mBuf;
-	iocbs[pos] = cb;
-	int rc = io_submit(mIoCtx, 1, &iocbs[0]);
+	for (;it != bufs.end();++it)
+	{
+		struct iocb* cb = &iocbArray[pos];
+		io_prep_pread( cb, fd, (it->mBuf)->mBuf, bufsize-1, (bufsize-1)*pos );
+		cb->data = (it->mBuf)->mBuf;
+		iocbs[pos] = cb;
+		pos++;
+	}
+	int rc = io_submit(mIoCtx,bufs.size(), &iocbs[0]);
 	if (rc != 1)
 	{
 		cout<<"msg: "<<strerror(errno)<<endl;
 	}
-//	cb->data = reinterpret_cast<void*>(buf.getInner());
-//	iocbs[pos] = cb;
-//	pos ++;
-//	for (;it != bufs.end(); ++ it)
+//	struct io_event events[1024];
+//	rc = io_getevents(mIoCtx,1,1024,events,NULL);
+//	printf("finish %d io_events\n",rc);
+////	cout<<reinterpret_cast<char*>(events[0].data)<<endl;
+//	for (int i = 0;i<rc;i++)
 //	{
-//		BufferUser& buf = *it;
-//		struct iocb* cb = &iocbArray[pos];
-//		io_prep_pread(cb, buf);
+//		printf("this is [%d] block, size[%d]\n",i+1, strlen(reinterpret_cast<char*>(events[i].data)));
+//		cout<<reinterpret_cast<char*>(events[i].data)<<endl;
 //	}
 }
 
 void Center::aioreap()
 {
+	struct timespec ts;
+	ts.tv_sec = 1;
+	ts.tv_nsec = 0;
+	const size_t MAX_EVENT_COUNT = 1204;
+	struct io_event events[ MAX_EVENT_COUNT ];
+	int rc = io_getevents( mIoCtx, 1, MAX_EVENT_COUNT, events, &ts );
+	printf("finish %d io_events\n",rc);
+	for(int i = 0;i<rc;i++)
+	{
+		 printf("this is block [%d], size[%d]\n",i+1, strlen(reinterpret_cast<char*>(events[i].data)));
+		 cout<<reinterpret_cast<char*>(events[i].data)<<endl;
+	}
 
 }
